f26ba351d1aada85a3710ebca45ff2850a5bbec2
elisolero <eli.solero.web@gmail.com>
2019-02-07 13:24:04 +0200

    add component with button

---
+++ components/FetchLocation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+const fetchLocation = props => {
+    return (
+        <Button title="get location" onPress={props.onGetLocation} />
+    );
+};
+
+export default fetchLocation;
