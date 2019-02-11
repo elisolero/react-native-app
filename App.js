a8090317f5b4767bbacdb756c45e9dde3bf20f6f
elisolero <eli.solero.web@gmail.com>
2019-02-11 13:37:09 +0200

    fix ios files

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View,Alert} from 'react-native';
 import FetchLocation from './components/FetchLocation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
   userLocationHandler = () => {
     // console.log('Button clicked!!');
     navigator.geolocation.getCurrentPosition(pos => {
@@ -27,7 +12,7 @@ export default class App extends Component<Props> {
     },err => console.log(err))
   }
 
-  
+
   render() {
 
     return (
