8b06d9f9e41c9f08634702df86ca4b0449943dbf
elisolero <eli.solero.web@gmail.com>
2019-02-11 14:04:13 +0200

    Map Working

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View,Alert} from 'react-native';
 import FetchLocation from './components/FetchLocation';
-
+import MapView from 'react-native-maps';
 
 export default class App extends Component{
   userLocationHandler = () => {
@@ -11,14 +11,24 @@ export default class App extends Component{
       console.log(pos)
     },err => console.log(err))
   }
-
+  
 
   render() {
 
     return (
-      <View style={styles.container}>
-        <FetchLocation getlocation={this.userLocationHandler} />
-      </View>
+   <View style={styles.container}>
+     {/* <FetchLocation getlocation={this.userLocationHandler} /> */}
+     <MapView
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+   </View>
     );
   }
 }
@@ -29,6 +39,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
   welcome: {
     fontSize: 20,
